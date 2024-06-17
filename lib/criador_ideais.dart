e8847f06fdee946e0cac95e4666512482dc0dec8
Pedro Casteli <pedro.casteli@zaffarinet.com.br>
2024-06-17 16:54:52 -0300

    arquivo inicial

---
+++ lib/criador_ideais.dart
@@ -0,0 +1,7 @@
+class CriadorIdeias {
+  final String genero;
+  final String lugar;
+  final String personagem;
+
+  CriadorIdeias({required this.genero, required this.lugar, required this.personagem});
+}
